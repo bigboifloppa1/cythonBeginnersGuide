d6f63f9aa61a7f99349f1dc0299ae15ef349ba5e
bigboifloppa1 <101159417+bigboifloppa1@users.noreply.github.com>
2024-04-07 10:58:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -34,6 +34,6 @@ getstarted.addEventListener("click", () => {
   }, 300)
   setTimeout(function() {
     block.style.removeProperty("animation");
-    window.location.href = "cythonTut.html"
+    window.location.href = "https://bigboifloppa1.github.io/cythonbeginnersguide/cythonTut.html"
   }, 500)
 });
